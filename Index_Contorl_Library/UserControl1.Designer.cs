f781a813b203d8ffe737b92f896ebe9c9defcccf
eric025 <lx20040211@gmail.com>
2019-04-06 21:47:10 +0800

    Finished Div 1.0

--- Index_Contorl_Library/UserControl1.Designer.cs
+++ Index_Contorl_Library/UserControl1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Index_Contorl_Library
 {
-    partial class UserControl1
+    partial class IC_NumberChangeBox
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -27,12 +27,67 @@
         /// 使用代码编辑器修改此方法的内容。
         /// </summary>
         private void InitializeComponent()
+
         {
-            components = new System.ComponentModel.Container();
+            this.but_Reduce = new System.Windows.Forms.Button();
+            this.but_Add = new System.Windows.Forms.Button();
+            this.txt_Number = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // but_Reduce
+            // 
+            this.but_Reduce.Font = new System.Drawing.Font("宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but_Reduce.Location = new System.Drawing.Point(4, 10);
+            this.but_Reduce.Margin = new System.Windows.Forms.Padding(4);
+            this.but_Reduce.Name = "but_Reduce";
+            this.but_Reduce.Size = new System.Drawing.Size(61, 49);
+            this.but_Reduce.TabIndex = 0;
+            this.but_Reduce.Text = "←";
+            this.but_Reduce.UseVisualStyleBackColor = true;
+            this.but_Reduce.Click += new System.EventHandler(this.But_Reduce_Click);
+            // 
+            // but_Add
+            // 
+            this.but_Add.Font = new System.Drawing.Font("宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but_Add.Location = new System.Drawing.Point(220, 12);
+            this.but_Add.Margin = new System.Windows.Forms.Padding(4);
+            this.but_Add.Name = "but_Add";
+            this.but_Add.Size = new System.Drawing.Size(65, 49);
+            this.but_Add.TabIndex = 0;
+            this.but_Add.Text = "→";
+            this.but_Add.UseVisualStyleBackColor = true;
+            this.but_Add.Click += new System.EventHandler(this.But_Add_Click);
+            // 
+            // txt_Number
+            // 
+            this.txt_Number.Font = new System.Drawing.Font("Comic Sans MS", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Number.Location = new System.Drawing.Point(72, 3);
+            this.txt_Number.Name = "txt_Number";
+            this.txt_Number.Size = new System.Drawing.Size(141, 63);
+            this.txt_Number.TabIndex = 1;
+            this.txt_Number.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_Number.TextChanged += new System.EventHandler(this.Txt_Number_TextChanged);
+            // 
+            // IC_NumberChangeBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_Number);
+            this.Controls.Add(this.but_Add);
+            this.Controls.Add(this.but_Reduce);
+            this.Font = new System.Drawing.Font("宋体", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "IC_NumberChangeBox";
+            this.Size = new System.Drawing.Size(294, 73);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button but_Reduce;
+        private System.Windows.Forms.Button but_Add;
+        private System.Windows.Forms.TextBox txt_Number;
     }
 }
--- Index_Contorl_Library/UserControl1.cs
+++ Index_Contorl_Library/UserControl1.cs
@@ -10,11 +10,77 @@ using System.Windows.Forms;
 
 namespace Index_Contorl_Library
 {
-    public partial class UserControl1: UserControl
+    public partial class IC_NumberChangeBox : UserControl
     {
-        public UserControl1()
+        public IC_NumberChangeBox()
         {
             InitializeComponent();
+            Number = 1;
+            txt_Number.Text = Number.ToString();
+        }
+        public delegate void NumberChangedHandler(object sender, EventArgs e);
+        public event NumberChangedHandler NumberChanged;
+        public int max;
+        public int GetMax()
+        {
+            return max;
+        }
+
+        public void SetMax(int max)
+        {
+            this.max = max;
+            NumberChange();
+        }
+        public int Number { get; set; }
+        protected void But_Reduce_Click(object sender, EventArgs e)
+        {
+            Number--;
+            NumberChange();
+        }
+        private void NumberChange()
+        {
+            but_Add.Enabled = true;
+            but_Reduce.Enabled = true;
+            if (Number >= max)
+            {
+                but_Add.Enabled = false;
+                Number = max;
+            }
+            if (Number <= 1)
+            {
+                but_Reduce.Enabled = false;
+                Number = 1;
+            }
+            txt_Number.Text = Number.ToString();
+            if (NumberChanged != null)
+            {
+                NumberChanged(this, new EventArgs());
+            }
+        }
+        protected void But_Add_Click(object sender, EventArgs e)
+        {
+            Number++;
+            NumberChange();
+        }
+        protected void Txt_Number_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Number = int.Parse(txt_Number.Text);
+            }
+            catch
+            {
+                if (txt_Number.Text != "")
+                {
+                    MessageBox.Show("请输入正确的数字", "错误！");
+                }
+                else
+                {
+                    txt_Number.Text = "";
+                    Number = 0;
+                }
+            }
+            NumberChange();
         }
     }
 }
